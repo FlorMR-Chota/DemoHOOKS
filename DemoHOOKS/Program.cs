d7a270486c66cda54d03ddd38d8e9dbae0ba18f7
Deysi Muñoz <deysiflormunozrojas@gmail.com>
2024-11-28 10:47:12 -0500

    Demostracion de hoock formado de codigo

--- DemoHOOKS/Program.cs
+++ DemoHOOKS/Program.cs
@@ -1,2 +1,12 @@
 ﻿
 Console.WriteLine("Hello, World!");
+Console.WriteLine(" Prueva de formateo de codigo");
+int edad = 25;
+if (edad >= 18)
+   {
+          Console.WriteLine("Es mayor de edad");
+                    }
+  else
+             {
+    Console.WriteLine("Es menor de edad");
+  }
